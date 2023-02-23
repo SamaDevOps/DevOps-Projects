2b34fc49b4e38175e0a62db30c868e7cfe3af88b
SamaDevOps <samadi130@gmail.com>
2023-02-23 11:26:35 +0530

    DevOps Projects

---
+++ jenkins/Jenkins/saha-rajdeep-lab1/pipeline-code/app.py
@@ -0,0 +1,6 @@
+
+def my_hello_world():
+    print("Hello World!!!!!! 123123")
+    
+    return "Hello World !!!!!!!!!!!! 321321"
+my_hello_world()
---
+++ python/Python/100-days-of-python/Day-1/main.py
@@ -0,0 +1,26 @@
+print("Hello World!!!")
+
+print("\n")
+
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')")
+
+print("\n")
+
+print("Hello world\nHello world\nHello world")
+
+print("\n")
+
+print("Hello" + " " + "Samadhi!")
+
+print("\n")
+
+print("Day 1 - String Manipulation")
+print('String Concatenation is done with the "+" sign')
+print('e.g print("Hello" + "world")')
+print("New lines can be created with a backslash and n.")
+
+print("\n")
+
+print("Hello " + input("What is your name? "))
