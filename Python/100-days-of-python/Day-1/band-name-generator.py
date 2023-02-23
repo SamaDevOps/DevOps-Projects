5fee893646271ab3f2feae9102a415d00593000a
SamaDevOps <samadi130@gmail.com>
2023-02-23 13:35:00 +0530

    Day 1 training and final challange

---
+++ Python/100-days-of-python/Day-1/band-name-generator.py
@@ -0,0 +1,11 @@
+#greeting
+print("Hello this is trusty little Band Name Generator\n")
+
+#Ask for city
+city = input("What is the city you grew up in?\n")
+
+#Ask for the name of a pet
+pet = input("What is your pets name?\n")
+
+#combine names
+print("Your band name is: " + city + pet)
--- Python/100-days-of-python/Day-1/main.py
+++ Python/100-days-of-python/Day-1/main.py
@@ -1,21 +1,26 @@
+#Hello World! here I come!
 print("Hello World!!!")
 
 print("\n")
 
+#How to use " " and ''
 print("Day 1 - Python Print Function")
 print("The function is declared like this:")
 print("print('what to print')")
 
 print("\n")
 
+#newline
 print("Hello world\nHello world\nHello world")
 
 print("\n")
 
+#Concatanation
 print("Hello" + " " + "Samadhi!")
 
 print("\n")
 
+#debugging
 print("Day 1 - String Manipulation")
 print('String Concatenation is done with the "+" sign')
 print('e.g print("Hello" + "world")')
@@ -23,8 +28,34 @@ print("New lines can be created with a backslash and n.")
 
 print("\n")
 
+#input function
 print("Hello " + input("What is your name? "))
 
 print("\n")
 
-# To comment-out shortcut "ctrl+/"
+# Comments: To comment-out shortcut "ctrl+/"
+
+#len function nested functions
+print( len( input("What is your NAME?? ") ) )
+
+print("\n")
+
+#variables
+name = input("What is your name? ")
+length = len(name)
+print(length)
+
+print("\n")
+
+#code exercise 1.4 switch variable values
+
+a = input("a: ")
+b = input("b: ")
+
+c = a
+a = b
+b = c
+
+
+print("a = " + a)
+print("b = " + b)
