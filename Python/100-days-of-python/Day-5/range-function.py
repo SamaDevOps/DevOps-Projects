c64a937cfc12f4c41fb1dec78e027f6d9623d623
SamaDevOps <samadi130@gmail.com>
2023-03-05 13:58:07 +0530

    range fucntion with for loop

---
+++ Python/100-days-of-python/Day-5/range-function.py
@@ -0,0 +1,16 @@
+import random
+total = 0
+for number in range(1, 101):
+    total += number
+
+print(total)
+    
+#testing
+
+# english_alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+
+# charactor_needed = 5
+
+# for number in range(1, charactor_needed+1):
+#     random_number = random.randint(1,len(english_alphabet))
+#     print(english_alphabet[random_number])
