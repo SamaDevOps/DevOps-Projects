8308034674267817cf8a21296b286c2033780fb5
SamaDevOps <samadi130@gmail.com>
2023-03-12 18:45:49 +0530

    caesar cipher code improved

---
+++ Python/100-days-of-python/Day-8/caesar-better.py
@@ -0,0 +1,21 @@
+import math
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+def caesar(start_text, shift_amount, cipher_direction):
+    end_text = ""
+    if cipher_direction == "decode":
+            shift_amount *= -1
+    for letter in start_text:
+        position = alphabet.index(letter)
+        new_position = position + shift_amount
+        end_text += alphabet[new_position]
+    print(f"The {cipher_direction}d text is {end_text}")
+    
+
+    
+start_text = input("Enter start text : ")
+shift_amount = int(input("Enter shift amount : "))
+cipher_direction = input("Enter encode or decode : ")
+
+caesar(start_text=start_text, shift_amount=shift_amount, cipher_direction=cipher_direction)
