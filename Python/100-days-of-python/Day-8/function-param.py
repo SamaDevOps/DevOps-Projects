d8ae646b7f0ebaafe3cc9dd93dfe77af92fceb0b
SamaDevOps <samadi130@gmail.com>
2023-03-12 10:11:08 +0530

    changed the comments

--- Python/100-days-of-python/Day-8/function-param.py
+++ Python/100-days-of-python/Day-8/function-param.py
@@ -1,7 +1,27 @@
-def greet(name):
-    print(f"Hey {name}")
-    print("How are you?")
+# def greet(name):
+#     print(f"Hey {name}")
+#     print("How are you?")
     
-name_input = input("Please enter you name : ")
+# name_input = input("Please enter you name : ")
 
-greet(name_input)
+# greet(name_input)
+
+
+
+
+
+# Functions with more positional arguments
+
+def greet_with(name,name2,time):
+    print(f"Hey {name} {name2}, {time}")
+    
+greet_with("Samadhi","Mabadawila","Good morning")
+
+
+
+# Functions with keyword arguments
+
+def greet_with_kyword(nam,nam2,time2):
+    print(f"Hey {nam} {nam2} {time2}")
+    
+greet_with_kyword(time2 = "Good night", nam2 = "maba", nam = "sama")
