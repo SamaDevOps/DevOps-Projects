c65395c6b3f2a037482943071b02f4194e8464c1
SamaDevOps <samadi130@gmail.com>
2023-03-19 14:28:13 +0530

    updated main.tf

--- Terraform/learn-terraform-aws-instance/main.tf
+++ Terraform/learn-terraform-aws-instance/main.tf
@@ -1,11 +1,12 @@
 terraform {
-  required_version = ">= 1.2.0"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 4.16"
     }
   }
+
+  required_version = ">= 1.2.0"
 }
 
 provider "aws" {
@@ -15,7 +16,8 @@ provider "aws" {
 resource "aws_instance" "app_server" {
   ami           = "ami-082b1f4237bd816a1"
   instance_type = "t2.micro"
+
   tags = {
-    Name = "EC2WithTerraform"
+    Name = var.instance_name
   }
 }
